8f4460ea54e896ed3dc087eaa5f9378d5f76e4dc
syzygy608 <hankwu99@gmail.com>
2022-04-15 00:21:04 +0800

    ac code update

---
+++ Code/ccupcxd/0415/A.cpp
@@ -0,0 +1,75 @@
+/* 
+ * Author: D1stance || Bey0nd
+ * Time: 2022-04-15 00:04:11
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define MiruSort ios_base::sync_with_stdio(0); cin.tie(0);
+#define mp make_pair
+#define eb emplace_back
+#define x first
+#define y second
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define loop(i, n) for(int i = 0; i < n; ++i)
+
+const ld pi = atan2(0, -1);
+const ld eps = 1e-4;
+
+const int maxn = 100005;
+struct Edge
+{
+    ll v, w;
+    bool operator < (const Edge &cmp) const
+    {
+        return cmp.w < w;
+    }
+};
+vector<Edge> Graph[maxn];
+ll dis[maxn];
+void dijkstra(int s)
+{
+    memset(dis, -1, sizeof(dis));
+    priority_queue<Edge> pq;
+    pq.push({s, 0});
+    while(!pq.empty())
+    {
+        auto node = pq.top(); pq.pop();
+        if(dis[node.v] != -1)
+            continue;
+        dis[node.v] = node.w;
+        for(auto k: Graph[node.v])
+        {
+            if(dis[k.v] == -1)
+                pq.push({k.v, node.w + k.w});
+        }
+    }
+}
+
+void sol()
+{
+	int n, m;
+    int u, v, w;
+    cin >> n >> m;
+    while(m--)
+    {
+        cin >> u >> v >> w;
+        Graph[u].push_back({v, w});
+    }
+    dijkstra(1);
+    for(int i = 1; i <= n; ++i)
+        cout << dis[i] << " \n"[i == n];
+}
+
+signed main()
+{
+    MiruSort
+    sol();
+    return 0;
+}
---
+++ Code/ccupcxd/0415/C.cpp
@@ -0,0 +1,64 @@
+/* 
+ * Author: D1stance || Bey0nd
+ * Time: 2022-04-15 00:04:11
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define MiruSort ios_base::sync_with_stdio(0); cin.tie(0);
+#define mp make_pair
+#define eb emplace_back
+#define x first
+#define y second
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define loop(i, n) for(int i = 0; i < n; ++i)
+
+const ld pi = atan2(0, -1);
+const ld eps = 1e-4;
+const int vertex = 505;
+
+ll dist[vertex][vertex];
+
+void sol()
+{
+	int n, m, q;
+    ll u, v, w;
+    cin >> n >> m >> q;
+    for(int i = 0; i <= n; i++)
+    {
+        for(int j = 0; j <= n; j++)
+            dist[i][j] = 1e18;
+        dist[i][i] = 0;
+    }
+    while(m--)
+    {
+        cin >> u >> v >> w;
+        u--, v--;
+        dist[u][v] = min(dist[u][v], w);
+        dist[v][u] = dist[u][v];
+    }
+    for(int k = 0; k < n; ++k)
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < n; ++j)
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+    int a, b;
+    while(q--)
+    {
+        cin >> a >> b;
+        a--, b--;
+        (dist[a][b] == 1e18) ? cout << "-1\n" : cout << dist[a][b] << '\n';
+    }
+}
+
+signed main()
+{
+    MiruSort
+    sol();
+    return 0;
+}
